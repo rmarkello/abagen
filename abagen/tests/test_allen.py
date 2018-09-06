bc79506789b6080a8111658751874ee9c7e71012
Ross Markello <rossmarkello@gmail.com>
2018-09-06 16:21:38 -0400

    [TEST] Fixes too-big tests
    Splits testing of `abagen.get_expression_data` into multiple tests;
    the data fetching _and_ testing in the same go was too much for poor
    Travis.

--- abagen/tests/test_allen.py
+++ abagen/tests/test_allen.py
@@ -6,14 +6,21 @@ from abagen.datasets import fetch_desikan_killiany
 ATLAS = fetch_desikan_killiany()
 
 
-def test_get_expression_data(testfiles):
-    kwargs = {}
-    for opts in [{}, {'atlas_info': ATLAS.info}, {'exact': False}]:
-        kwargs.update(opts)
-        out = allen.get_expression_data(testfiles, ATLAS.image, **kwargs)
-        assert isinstance(out, pd.DataFrame)
-        assert out.index.name == 'label'
-        assert out.columns.name == 'gene_symbol'
+def test_vanilla_get_expression_data(testfiles):
+    out = allen.get_expression_data(testfiles, ATLAS.image)
+    assert isinstance(out, pd.DataFrame)
+    assert out.index.name == 'label'
+    assert out.columns.name == 'gene_symbol'
 
     with pytest.raises(KeyError):
         allen.get_expression_data({'microarray': [1, 2, 3]}, ATLAS.image)
+
+
+def test_extra_get_expression_data(testfiles):
+    for opts in [{'atlas_info': ATLAS.info},
+                 {'exact': False},
+                 {'atlas_info': ATLAS.info, 'exact': False}]:
+        out = allen.get_expression_data(testfiles, ATLAS.image, **opts)
+        assert isinstance(out, pd.DataFrame)
+        assert out.index.name == 'label'
+        assert out.columns.name == 'gene_symbol'
