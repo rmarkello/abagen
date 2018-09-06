8a8a7d9d877bb07936f8987e8fc62e94ecfd6d52
Ross Markello <rossmarkello@gmail.com>
2018-09-06 10:26:51 -0400

    [TEST] Updates tests
    Adds more tests of `get_expression_data` to confirm different
    combinations of parameters still work. More updates needed!

--- abagen/tests/test_allen.py
+++ abagen/tests/test_allen.py
@@ -1,15 +1,19 @@
 import pandas as pd
 import pytest
 from abagen import allen
-from abagen.tests.utils import get_resource
+from abagen.datasets import fetch_desikan_killiany
 
-ATLAS = get_resource('atlas-desikankilliany.nii.gz')
-ATLAS_INFO = get_resource('atlas-desikankilliany.csv')
+ATLAS = fetch_desikan_killiany()
 
 
 def test_get_expression_data(testfiles):
-    out = allen.get_expression_data(testfiles, ATLAS, ATLAS_INFO, exact=False)
-    assert isinstance(out, pd.DataFrame)
+    kwargs = {}
+    for opts in [{}, {'atlas_info': ATLAS.info}, {'exact': False}]:
+        kwargs.update(opts)
+        out = allen.get_expression_data(testfiles, ATLAS.image, **kwargs)
+        assert isinstance(out, pd.DataFrame)
+        assert out.index.name == 'label'
+        assert out.columns.name == 'gene_symbol'
 
     with pytest.raises(KeyError):
-        allen.get_expression_data({'microarray': [1, 2, 3]}, ATLAS, ATLAS_INFO)
+        allen.get_expression_data({'microarray': [1, 2, 3]}, ATLAS.image)
