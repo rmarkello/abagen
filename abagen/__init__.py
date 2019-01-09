107278020bc445ac91e4878fd3f4131c08af311a
Ying-Qiu Zheng <yingqiuz@users.noreply.github.com>
2019-01-09 18:45:17 -0500

    import mouse module

--- abagen/__init__.py
+++ abagen/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ['__version__',  'io', 'get_expression_data',
            'keep_stable_genes', 'remove_distance',  'aggregate_donors',
-           'fetch_microarray', 'fetch_desikan_killiany']
+           'fetch_microarray', 'fetch_desikan_killiany', 'mouse']
 
 from .info import __version__
 
@@ -9,3 +9,4 @@ from abagen.correct import keep_stable_genes, remove_distance
 from abagen.datasets import fetch_microarray, fetch_desikan_killiany
 from abagen.process import aggregate_donors
 from abagen import io
+from abagen import mouse
