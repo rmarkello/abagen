e55df406e2d8672d053d399ee827dc082410f1b9
Yingqiu Zheng - A. Dagher Lab <yzheng@bic.mni.mcgill.ca>
2019-01-18 13:08:55 -0500

    add new tests

--- abagen/mouse/tests/test_mouse.py
+++ abagen/mouse/tests/test_mouse.py
@@ -1,6 +1,14 @@
-from ..mouse import get_experiment_id_from_gene
+from ..mouse import (
+    get_experiment_id_from_gene,
+    _get_single_unionization_attribute,
+    get_unionization_from_experiment,
+    get_unionization_from_gene
+)
+
 import pytest
 import numpy as np
+import requests
+from xml.etree import ElementTree as ET
 
 RANDOM_ID = -1
 RANDOM_STRING = 'random_string'
@@ -26,10 +34,19 @@ TEST_GENE_ID = {
     }  # no experiment ID found (while the gene is valid)
 }
 # test single experiment ID
+TEST_STRUCTURE = [182305713, 182305709,182305705, 15564]
 TEST_ATTRIBUTE = ['expression-energy', 'expression-density', 'sum-pixels']
 TEST_EXPERIMENT = {
-    69782969: {'expression-energy': np.array([]), },
-
+    69782969: {
+        'expression-energy': np.array([7.73432, 7.28206, 3.82741, 2.52219]),
+        'expression-density':np.array([0.0603072, 0.0553335, 0.0298628, 0.0188711]),
+        'sum-pixels':np.array([419628.0, 2238000.0, 3629050.0, 1534150000.0])
+    },
+    986:{
+        'expression-energy': np.array([1.00879, 1.40198, 2.34988, 2.729]),
+        'expression-density':np.array([0.00650952, 0.00896709, 0.015409, 0.0178506]),
+        'sum-pixels':np.array([195863.0, 1051230.0, 1440430.0, 998731000.0])
+    }
 }
 
 
@@ -99,6 +116,33 @@ def test_get_experiment_id_from_gene():
                 gene_id=gene,
                 slicing_direction=RANDOM_STRING
             )
+
+
+def test_get_single_unionization_attribute():
+    for experiment_id in TEST_EXPERIMENT:
+        url = "http://api.brain-map.org/api/v2/data/SectionDataSet/query.xml?id={}" \
+            "&include=structure_unionizes%28structure%29"\
+            .format(experiment_id)
+        r = requests.get(url)
+        root = ET.fromstring(r.content)
+        with pytest.raises(AttributeError):
+            _get_single_unionization_attribute(
+                root=root,
+                attr=RANDOM_STRING,
+                structure_list=TEST_STRUCTURE
+            )
+        vals = _get_single_unionization_attribute(
+            root=root, attr='expression-energy', structure_list=TEST_STRUCTURE
+        )
+        assert np.allclose(vals, TEST_EXPERIMENT[experiment_id]['expression-energy'])
+        vals = _get_single_unionization_attribute(
+            root=root, attr='expression-density', structure_list=TEST_STRUCTURE
+        )
+        assert np.allclose(vals, TEST_EXPERIMENT[experiment_id]['expression-density'])
+        vals = _get_single_unionization_attribute(
+            root=root, attr='sum-pixels', structure_list=TEST_STRUCTURE
+        )
+        assert np.allclose(vals, TEST_EXPERIMENT[experiment_id]['sum-pixels'])
 
 
 def test_get_unionization_from_experiment():
