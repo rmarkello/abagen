e46bb4e8088b4b8c5068a257cfe1b6c8cca68059
Ross Markello <rossmarkello@gmail.com>
2019-08-27 15:30:04 -0400

    [TEST] Adds CLI tests for output options
    Including --save-donors, --save-counts, and --stdout

--- abagen/tests/test_cli.py
+++ abagen/tests/test_cli.py
@@ -4,6 +4,7 @@ Tests for abagen.cli module
 """
 
 import os
+from pkg_resources import resource_filename
 import pytest
 
 from abagen.cli import run
@@ -25,12 +26,47 @@ def test_run_get_parser(atlas, testdir):
         parser.parse_args(['notanatlas.nii.gz'])
 
 
-def test_run_main(atlas, testdir):
+def test_run_main(capsys, atlas, testdir):
+    outputfile = os.path.join(str(testdir), 'abagen_expression.csv')
+
+    # check basic usage
     run.main([
         '--data-dir', testdir,
         '--donors', '12876', '15496',
-        '--output-file', os.path.join(str(testdir), 'abagen_expression.csv'),
+        '--output-file', outputfile,
         atlas['image']
     ])
+    assert os.path.exists(outputfile)
 
-    assert os.path.exists(os.path.join(str(testdir), 'abagen_expression.csv'))
+    # check that save donors/counts outputs desired files
+    run.main([
+        '--data-dir', testdir,
+        '--donors', '12876', '15496',
+        '--output-file', outputfile,
+        '--save-donors', '--save-counts',
+        atlas['image']
+    ])
+    for rep in ['_counts.csv', '_12876.csv', '_15496.csv']:
+        assert os.path.exists(outputfile.replace('.csv', rep))
+
+    # check stdout (BLARGH)
+    run.main([
+        '--data-dir', testdir,
+        '--donors', '12876', '15496',
+        '--output-file', outputfile,
+        '--stdout',
+        atlas['image']
+    ])
+    stdout = capsys.readouterr().out
+    with open(outputfile, 'r') as src:
+        assert stdout == src.read()
+
+
+def test_exec_run_fail():
+    executable = resource_filename('abagen', 'cli/run.py')
+
+    # need to set this otherwise it won't fail
+    __name__ = '__main__'  # noqa
+    with pytest.raises(RuntimeError):
+        with open(executable, 'r') as src:
+            exec(src.read())
