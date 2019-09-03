e0ad9091ba67d2cde5f5cd794ead8229cd7b4f60
Ross Markello <rossmarkello@gmail.com>
2019-09-03 16:26:29 -0400

    [DOC] Fix weird importing of abagen

--- abagen/datasets/__init__.py
+++ abagen/datasets/__init__.py
@@ -1,3 +1,8 @@
+"""
+Functions for fetching data relevant to the Allen Brain Atlas human microarray
+dataset
+"""
+
 __all__ = [
     'fetch_microarray', 'fetch_raw_mri', 'fetch_desikan_killiany',
     'fetch_gene_group', 'WELL_KNOWN_IDS', '_get_dataset_dir'
