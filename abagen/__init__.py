736f565baf43bc1b700d050721d688d3c34d1cc8
Ross Markello <rossmarkello@gmail.com>
2021-03-02 12:47:46 -0500

    [REF] Package level imports

--- abagen/__init__.py
+++ abagen/__init__.py
@@ -3,7 +3,9 @@ __all__ = [
     'io', 'mouse', 'get_expression_data', 'get_samples_in_mask',
     'keep_stable_genes', 'normalize_expression', 'remove_distance',
     'fetch_desikan_killiany', 'fetch_gene_group', 'fetch_microarray',
-    'fetch_raw_mri', 'fetch_rnaseq', 'fetch_freesurfer', 'fetch_donor_info'
+    'fetch_raw_mri', 'fetch_rnaseq', 'fetch_freesurfer', 'fetch_donor_info',
+    'leftify_atlas', 'relabel_gifti', 'annot_to_gifti', 'check_atlas',
+    'AtlasTree'
 ]
 
 from ._version import get_versions
@@ -18,3 +20,5 @@ from .correct import keep_stable_genes, normalize_expression, remove_distance
 from .datasets import (fetch_desikan_killiany, fetch_gene_group,
                        fetch_microarray, fetch_raw_mri, fetch_rnaseq,
                        fetch_freesurfer, fetch_donor_info)
+from .images import leftify_atlas, relabel_gifti, annot_to_gifti, check_atlas
+from .matching import AtlasTree
