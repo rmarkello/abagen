1abc76ffc7a95277a1644479a43f0a950f139289
Ross Markello <rossmarkello@gmail.com>
2021-03-05 12:51:00 -0500

    [DOC] Update pandas dep in readme

--- abagen/info.py
+++ abagen/info.py
@@ -56,7 +56,7 @@ Currently, ``abagen`` works with Python 3.6+ and requires a few dependencies:
 
     - nibabel
     - numpy (>=1.14.0)
-    - pandas, and
+    - pandas (>=0.25.0), and
     - scipy
 
 There are some additional (optional) dependencies you can install to speed up
