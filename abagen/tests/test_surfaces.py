0dca36ef88a1163356b551de0ccb9563a3b08e9f
Ross Markello <rossmarkello@gmail.com>
2021-03-22 21:31:00 -0400

    [TEST] More surfaces tests

--- abagen/tests/test_surfaces.py
+++ abagen/tests/test_surfaces.py
@@ -24,6 +24,15 @@ def surf():
 
 
 def test_make_surf_graph(surf):
+    coords = np.array([[0, 0, 0], [0, 0, 1], [0, 1, 1], [1, 1, 0], [1, 1, 1]])
+    triangles = np.array([[0, 1, 2], [0, 1, 3], [2, 3, 4]])
+    graph = surfaces.make_surf_graph(coords, triangles)
+    assert isinstance(graph, sparse.csr_matrix)
+    assert graph.shape[0] == coords.shape[0]
+    assert graph.nnz == 8
+    assert np.allclose(graph[[0, 1], [-1, -1]], 0)
+
+    # real graph
     coords, triangles = surf
     graph = surfaces.make_surf_graph(coords, triangles)
     assert isinstance(graph, sparse.csr_matrix)
@@ -31,6 +40,21 @@ def test_make_surf_graph(surf):
 
 
 def test_get_graph_distance(surf):
+    coords = np.array([[0, 0, 0], [0, 0, 1], [0, 1, 1], [1, 1, 0], [1, 1, 1]])
+    triangles = np.array([[0, 1, 2], [0, 1, 3], [2, 3, 4]])
+    graph = surfaces.make_surf_graph(coords, triangles)
+    dist = surfaces.get_graph_distance(graph)
+    assert dist.shape == (5, 5)
+    assert dist[0, 4] == dist[0, 2] + dist[2, 4]
+
+    dist = surfaces.get_graph_distance(graph, nodes=[0, 1])
+    assert dist.shape == (2, 5)
+
+    dist = surfaces.get_graph_distance(graph, nodes=[0, 1],
+                                       labels=[1, 1, 1, 2, 2])
+    assert dist.shape == (2, 2)
+
+    # real graph
     graph = surfaces.make_surf_graph(*surf)
     dist = surfaces.get_graph_distance(graph, nodes=[0, 1, 2, 3])
     assert dist.shape == (4, graph.shape[1])
