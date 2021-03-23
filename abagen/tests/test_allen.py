3d5551a08db9c631629440c7aa4ac6aa45e293ff
Ross Markello <rossmarkello@gmail.com>
2021-03-22 21:10:33 -0400

    [TEST] Add test for get_interpolated_maps

--- abagen/tests/test_allen.py
+++ abagen/tests/test_allen.py
@@ -3,25 +3,24 @@
 Tests for abagen.allen module
 """
 
+import nibabel as nib
 import numpy as np
 import pandas as pd
 import pytest
 
 from abagen import allen
-from abagen.images import check_img
+from abagen.images import check_img, check_surface
 
 
 def test_vanilla_get_expression_data(testfiles, atlas):
-    out = allen.get_expression_data(atlas['image'],
-                                    donors=['12876', '15496'])
+    out = allen.get_expression_data(atlas['image'], donors=testfiles.keys())
     assert np.allclose(out.index, range(1, 84))
     assert out.index.name == 'label'
     assert out.columns.name == 'gene_symbol'
 
 
 def test_vanilla_surface_expression_data(testfiles, surface):
-    out = allen.get_expression_data(surface['image'],
-                                    donors=['12876', '15496'])
+    out = allen.get_expression_data(surface['image'], donors=testfiles.keys())
     assert out.index.name == 'label'
     assert out.columns.name == 'gene_symbol'
 
@@ -35,7 +34,7 @@ def test_extra_get_expression_data(testfiles, atlas, opts):
     if opts.get('atlas_info'):
         opts['atlas_info'] = atlas['info']
 
-    out = allen.get_expression_data(atlas['image'], donors=['12876', '15496'],
+    out = allen.get_expression_data(atlas['image'], donors=testfiles.keys(),
                                     **opts)
     assert out.index.name == 'label'
     assert out.columns.name == 'gene_symbol'
@@ -43,14 +42,14 @@ def test_extra_get_expression_data(testfiles, atlas, opts):
 
 def test_individualized_get_expression_data(testfiles):
     atlas = allen.datasets.fetch_desikan_killiany(native=True)
-    out = allen.get_expression_data(atlas['image'], donors=['12876', '15496'])
+    out = allen.get_expression_data(atlas['image'], donors=testfiles.keys())
     assert out.index.name == 'label'
     assert out.columns.name == 'gene_symbol'
 
 
 def test_individualized_surface_expression_data(testfiles):
     atlas = allen.datasets.fetch_desikan_killiany(native=True, surface=True)
-    out = allen.get_expression_data(atlas['image'], donors=['12876', '15496'])
+    out = allen.get_expression_data(atlas['image'], donors=testfiles.keys())
     assert out.index.name == 'label'
     assert out.columns.name == 'gene_symbol'
 
@@ -58,12 +57,13 @@ def test_individualized_surface_expression_data(testfiles):
 def test_get_expression_data_errors(testfiles, atlas):
     # invalid probe_selection method
     with pytest.raises(ValueError):
-        allen.get_expression_data(atlas['image'], donors=['12876', '15496'],
+        allen.get_expression_data(atlas['image'], donors=testfiles.keys(),
                                   probe_selection='nonsense')
 
     # cannot use diff_stability with only one donor
     with pytest.raises(ValueError):
-        allen.get_expression_data(atlas['image'], donors=['12876'],
+        donor = list(testfiles.keys())[0]
+        allen.get_expression_data(atlas['image'], donors=[donor],
                                   probe_selection='diff_stability')
 
 
@@ -86,7 +86,7 @@ def test_missing_labels(testfiles, atlas):
     out, counts = allen.get_expression_data(img, info,
                                             missing='centroids',
                                             return_counts=True,
-                                            donors=['12876', '15496'])
+                                            donors=testfiles.keys())
     assert out.index.name == 'label'
     assert out.columns.name == 'gene_symbol'
     assert len(out) == len(info)
@@ -96,9 +96,9 @@ def test_missing_labels(testfiles, atlas):
 
 
 def test_get_samples_in_mask(testfiles, atlas):
-    allexp, allcoords = allen.get_samples_in_mask(donors=['12876', '15496'])
+    allexp, allcoords = allen.get_samples_in_mask(donors=testfiles.keys())
     cortexp, cortcoords = allen.get_samples_in_mask(mask=atlas['image'],
-                                                    donors=['12876', '15496'])
+                                                    donors=testfiles.keys())
 
     # exp + coords shape as expected?
     assert len(allexp) == len(allcoords) and len(cortexp) == len(cortcoords)
@@ -109,3 +109,24 @@ def test_get_samples_in_mask(testfiles, atlas):
 
     # providing the cortical mask (atlas) should reduce # of samples returned
     assert len(allexp) > len(cortexp)
+
+
+def test_get_interpolated_map(testfiles, atlas, surface):
+    img = np.asarray(nib.load(atlas['image']).dataobj)
+    out = allen.get_interpolated_map('A1BG', mask=atlas['image'],
+                                     donors=testfiles.keys())
+    assert isinstance(out, dict)
+    assert len(out) == 1
+    assert 'A1BG' in out
+    assert out['A1BG'].shape == img.shape
+    assert np.allclose(out['A1BG'].nonzero(), img.nonzero())
+
+    img = check_surface(surface['image'])[0]
+    out = allen.get_interpolated_map(('A1BG', 'ZZZ3'), mask=surface['image'],
+                                     donors=testfiles.keys())
+    assert isinstance(out, dict)
+    assert len(out) == 2
+    for f in ('A1BG', 'ZZZ3'):
+        assert f in out
+        assert out[f].shape == img.shape
+        assert np.allclose(out[f].nonzero(), img.nonzero())
