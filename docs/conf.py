28e238cf6a12ecb3a8fde0abb70ab0b6e9108394
Ross Markello <rossmarkello@gmail.com>
2021-03-29 14:39:37 -0400

    [DOC] Fix logo [skip ci]

--- docs/conf.py
+++ docs/conf.py
@@ -95,7 +95,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = '_static/abagen_logo.svg'
+html_logo = '_static/abagen_logo_clear.png'
 
 # https://github.com/rtfd/sphinx_rtd_theme/issues/117
 def setup(app):  # noqa
